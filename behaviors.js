0adbfab7f5708a923e8b770c3c0c0010cc3bb665
Brad Czerniak <ao5357@gmail.com>
2016-05-09 22:42:43 -0400

    Update behaviors.js

--- behaviors.js
+++ behaviors.js
@@ -3,7 +3,7 @@
  * jQuery when the DOM is ready.
  */
 $(document).ready(function(){
-  var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f'?q=Breakups;
+  var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f?q=Breakups';
 
   $.getJSON(url, function(tracks) {
     $(tracks).each(function(track) {
