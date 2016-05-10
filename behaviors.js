6b5748a7f7f02593c0ccd645f6199dccac0c0c40
Brad Czerniak <ao5357@gmail.com>
2016-05-09 22:50:09 -0400

    Update behaviors.js

--- behaviors.js
+++ behaviors.js
@@ -3,7 +3,7 @@
  * jQuery when the DOM is ready.
  */
 $(document).ready(function(){
-  var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f?q=Breakups';
+  var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f&q=Breakups';
 
   $.getJSON(url, function(tracks){
     $.each(tracks, function(i, track){
@@ -12,6 +12,20 @@ $(document).ready(function(){
         'track': track
       });
     });
+  });
+
+  
+  SC.initialize({
+    client_id: '59a99891c97851fe05153a2c4afeff7f'
+  });
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'buskers', 
+  license: 'cc-by-sa'
+})
+  .then(function(tracks) {
+    console.log(tracks);
   });
 });
 
