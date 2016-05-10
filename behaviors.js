e22e2e686ca0612ff152f681cb5d35d393272d1a
Brad Czerniak <ao5357@gmail.com>
2016-05-09 22:38:10 -0400

    Create behaviors.js

---
+++ behaviors.js
@@ -0,0 +1,7 @@
+var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f'?q=Breakups;
+
+$.getJSON(url, function(tracks) {
+  $(tracks).each(function(track) {
+    console.log(track);
+  }
+});
