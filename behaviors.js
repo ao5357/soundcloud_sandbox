fd6c95ec84aef4cecb73ff4a1b5c0792a6ecd9cf
Brad Czerniak <ao5357@gmail.com>
2016-05-09 23:01:55 -0400

    Update behaviors.js

--- behaviors.js
+++ behaviors.js
@@ -3,29 +3,28 @@
  * jQuery when the DOM is ready.
  */
 $(document).ready(function(){
-  var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f&q=Breakups';
-
-  $.getJSON(url, function(tracks){
-    $.each(tracks, function(i, track){
-      console.log({
-        'i': i,
-        'track': track
-      });
-    });
-  });
-
-  
   SC.initialize({
     client_id: '59a99891c97851fe05153a2c4afeff7f'
   });
 
-  // find all sounds of buskers licensed under 'creative commons share alike'
-  SC.get('/tracks', {
-    q: 'buskers'
-  })
-    .then(function(tracks) {
-      console.log(tracks);
+  var $form = $('form'),
+    $results = $('#results');
+  $form.on('submit', function(e){
+    $results.html('Loading...');
+    
+    var resultHtml = '<ul>';
+    // find all sounds of buskers licensed under 'creative commons share alike'
+    SC.get('/tracks', {
+      q: $form.find('#search').val()
+    })
+      .then(function(tracks) {
+        $.each(tracks, function(i, track){
+          resultHtml += '<li><a href="' + track.permalink_url + '">' + track.title + '</a></li>';
+        });
+        $results.html(resultHtml + '</ul>');
+      });
     });
+    e.stopPropagation();
+    return false;
   });
-
 })(jQuery);
