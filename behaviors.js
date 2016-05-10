b622650af77efe8c1fc1ac01492b016919ff3360
Brad Czerniak <ao5357@gmail.com>
2016-05-09 23:05:48 -0400

    Update behaviors.js

--- behaviors.js
+++ behaviors.js
@@ -9,22 +9,25 @@ $(document).ready(function(){
 
   var $form = $('form'),
     $results = $('#results');
+
   $form.on('submit', function(e){
+    e.stopPropagation();
     $results.html('Loading...');
     
     var resultHtml = '<ul>';
     // find all sounds of buskers licensed under 'creative commons share alike'
-    SC.get('/tracks', {
-      q: $form.find('#search').val()
-    })
+    SC
+      .get('/tracks', {
+        q: $form.find('#search').val()
+      })
       .then(function(tracks) {
         $.each(tracks, function(i, track){
           resultHtml += '<li><a href="' + track.permalink_url + '">' + track.title + '</a></li>';
         });
         $results.html(resultHtml + '</ul>');
       });
-    });
-    e.stopPropagation();
+
     return false;
   });
+});
 })(jQuery);
