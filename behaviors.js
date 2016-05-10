479203c628cf89300bad0ace25c55bc8d29d3e4b
Brad Czerniak <ao5357@gmail.com>
2016-05-09 22:42:11 -0400

    Update behaviors.js

--- behaviors.js
+++ behaviors.js
@@ -1,7 +1,15 @@
-var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f'?q=Breakups;
+(function($) {
+/**
+ * jQuery when the DOM is ready.
+ */
+$(document).ready(function(){
+  var url = 'https://api.soundcloud.com/tracks?client_id=59a99891c97851fe05153a2c4afeff7f'?q=Breakups;
 
-$.getJSON(url, function(tracks) {
-  $(tracks).each(function(track) {
-    console.log(track);
-  }
+  $.getJSON(url, function(tracks) {
+    $(tracks).each(function(track) {
+      console.log(track);
+    }
+  });
 });
+
+})(jQuery);
